6d02e254d10df5411736d9b54a00a6167b1f2efc
LuisFerGeronimo <gclo151861@upemor.edu.mx>
2018-10-15 12:25:15 -0500

    REINICIO/LIMPIEZA de repositorio

--- includes/topHtml.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Dymo <?php if($paginaActual!="Inicio"){ echo "- ".$paginaActual;} ?></title>
-	<link rel="stylesheet" type="text/css" href="styles/style.css">
-	<link rel="stylesheet" type="text/css" href="lightbox.css">
-</head>
